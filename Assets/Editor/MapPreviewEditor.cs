68bb03f2a8f42d2991afc0e4fad6d83403e6b487
IIMasterXII <erich.showalter@gmail.com>
2020-02-15 22:01:42 -0800

    unity project files

---
+++ Assets/Editor/MapPreviewEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapPreview))]
+public class MapPreviewEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapPreview mapPrev = (MapPreview)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapPrev.autoUpdate)
+            {
+                mapPrev.DrawMapInEditor();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapPrev.DrawMapInEditor();
+        }
+    }
+}
